ddd42ac6d8ad972ba6e3adc39aeb0c63d10bb9c7
naowtc <naowtc@gmail.com>
2020-09-03 19:17:30 +0900

    first commit

---
+++ db/migrate/20200822111140_create_posts.rb
@@ -0,0 +1,18 @@
+# class CreatePosts < ActiveRecord::Migration[6.0]
+#   def change
+#     create_table :posts do |t|
+#       t.text :content
+#       t.timestamps
+#     end
+#   end
+# end
+
+class CreatePosts <ActiveRecord::Migration[6.0] 
+  def change
+    create_table :posts do |t|
+      t.text :memo
+      t.timestamps
+    end
+  end
+end
+
